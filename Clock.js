402cb17447b6f1ca115bc093a5bdea394404b618
yaminm <yamin.menash@gmail.com>
2017-02-20 23:12:01 +0200

    intelj git commit

---
+++ Clock.js
@@ -0,0 +1,36 @@
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  componentDidMount() { //run first time the component get to the DOM
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() { //run when the component removed from the DOM
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Clock />,
+  document.getElementById('clock')
+);
